a9893cae5345ff3265657ef9407e151a8cccbf67
bilal ahmed <ba348861@gmail.com>
2022-01-17 22:52:45 +0300

    7th commit dealt with the additi0n and the minus

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const navLinks = document.querySelector('.nav_links');
 const previousBtn = document.querySelector('.previous');
 const nextBtn = document.querySelector('.next');
 const images = document.querySelector('.images');
+const Plus = document.querySelector('.plus');
+const Minus = document.querySelector('.minus');
+const Output = document.querySelector('.output');
 
 
 
@@ -32,4 +35,22 @@ previousBtn.addEventListener('click' ,() =>{
         counter = arr.length-1;
     }
     images.src = arr[counter];
+})
+
+
+// adding anmd minusing from the cart
+ let calculating = 0;
+
+Plus.addEventListener('click' ,() =>{
+      calculating++;
+
+      Output.innerHTML = calculating;
+});
+
+Minus.addEventListener('click' ,() =>{
+    calculating--;
+    if(calculating < 0 ){
+        calculating = calculating * 0;
+    }
+    Output.innerHTML = calculating;
 })
