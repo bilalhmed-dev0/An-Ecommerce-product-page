45002530cb7d8a99a56802192bf2a5200d8f0827
bilal ahmed <ba348861@gmail.com>
2022-01-17 21:18:21 +0300

    5th commit i dealt with the image slider

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const menu_Btn = document.querySelector('.menu-btn');
 const navLinks = document.querySelector('.nav_links');
+const previousBtn = document.querySelector('.previous');
+const nextBtn = document.querySelector('.next');
+const images = document.querySelector('.images');
+
 
 
 menu_Btn.addEventListener('click', () => {
@@ -9,3 +13,23 @@ menu_Btn.addEventListener('click', () => {
    
     
 });
+
+let arr = ["images/image-product-1.jpg" , "images/image-product-2.jpg" , "images/image-product-3.jpg" , "images/image-product-4.jpg"];
+let counter = 0;
+
+nextBtn.addEventListener('click' ,() =>{
+    counter++;
+    if(counter > arr.length -1){
+        counter=0;
+    }
+    images.src = arr[counter];
+});
+
+
+previousBtn.addEventListener('click' ,() =>{
+    counter--;
+    if(counter < 0){
+        counter = arr.length-1;
+    }
+    images.src = arr[counter];
+})
